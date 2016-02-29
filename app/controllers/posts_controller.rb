ac891448b3b372a7237a61ac8c4dabcb9b6e2ce3
mafelix <felixmdev@gmail.com>
2016-02-29 03:18:41 +0000

    5 done. Watch ur typos

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,8 +39,11 @@ class PostsController < ApplicationController
     ]
   end
 
-  def post0
-    @post = {
+
+  def show
+    
+    posts = [
+      {
         title: "Superstar",
         author: "Carly Rae Jepson",
         text: <<-eos.gsub(/\s+/, " ").strip
@@ -49,11 +52,8 @@ class PostsController < ApplicationController
           is unparalleled. And those costumes! He remains an inspiration to this
           day.
         eos
-      } 
-  end
-
-  def post1
-    @post = {
+      },
+      {
         title: "Basketball Idol",
         author: "Steve Nash",
         text: <<-eos.gsub(/\s+/, " ").strip
@@ -61,11 +61,8 @@ class PostsController < ApplicationController
           pickup games on the street. Dude had moves nobody had ever seen. Breaking
           ankles. Poppin' threes. Great all-around game.
         eos
-      }
-    end
-
-    def post2
-      @post = {
+      },
+      {
         title: "Acting Legend",
         author: "Michael J. Fox",
         text: <<-eos.gsub(/\s+/, " ").strip
@@ -73,14 +70,16 @@ class PostsController < ApplicationController
           studied with the best and it shows. His acting chops were already legendary
           before his teaching career began. But it seems he's actually improved!
         eos
-      }
-    end
-
-    def post3
-      @post = {
+      },
+      {
         title: "Who?",
         author: "Vurram Khirani",
         text: "Never heard of this guy Khurram Virani, but he sounds great."
       }
-    end
-end
+    ]
+  
+    @post = posts[params[:id].to_i]
+  end
+  
+
+  end
